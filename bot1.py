150eb268c8255cdcae2c5d6205c9832f32b23506
edutvares <eduardoasst@gmail.com>
2023-09-29 11:32:58 -0300

    feat: Adicionar template

--- bot1.py
+++ bot1.py
@@ -1,12 +1,21 @@
 import os
 from dotenv import load_dotenv
+import string
 
 load_dotenv()
 
 BOT_API = os.getenv('BOT_API')
 SITE_BANCA = os.getenv('SITE_BANCA')
 
+with open('template.txt', 'r') as template_file:
+    template = template_file.read()
+
+template_obj = string.Template(template)
+
+substituicoes = {'BOT_API': BOT_API, 'SITE_BANCA': SITE_BANCA}
+
+resultado = template_obj.substitute(substituicoes)
+
 print("----------------------------------------------")
-print("Key: ", BOT_API)
-print("Site: ", SITE_BANCA)
+print(resultado)
 print("----------------------------------------------")
